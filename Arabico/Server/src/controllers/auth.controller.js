3082bb4683d38369ea4659b99bd78b8066fa2ec4
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-06-11 23:47:26 +0530

    fix: Prettier

--- Arabico/Server/src/controllers/auth.controller.js
+++ Arabico/Server/src/controllers/auth.controller.js
@@ -37,26 +37,33 @@ async function register(req, res) {
     }
 }
 
-async function login(req,res){
+async function login(req, res) {
     const { email, password } = req.body;
     const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
     if (!email || !password) {
         return res.status(400).json({ message: 'Email and password are required' });
     }
-    if(!token){
+    if (!token) {
         return res.status(401).json({ message: 'Session not found' });
     }
-    try {        const decoded = jwt.verify(token, config.jwtSecretKey);
+    try {
+        const decoded = jwt.verify(token, config.jwtSecretKey);
         if (!decoded || !decoded.userId) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
         }
         const user = await userModel.findOne({ email });
         if (!user) {
-            return res.status(400).json({ message: 'Invalid email or password' });
+            return res.status(400).json({
+                message: 'Invalid email or password'
+            });
         }
         const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
         if (user.password !== hashedPassword) {
-            return res.status(400).json({ message: 'Invalid email or password' });
+            return res.status(400).json({
+                message: 'Invalid email or password'
+            });
         }
         const newToken = jwt.sign({ userId: user._id },
             config.jwtSecretKey,
--- Arabico/Server/src/middlewares/auth.middleware.js
+++ Arabico/Server/src/middlewares/auth.middleware.js
@@ -6,16 +6,23 @@ const authControllerMiddleware = async (req, res, next) => {
     try {
         const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
         if (!token) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({ 
+                message: 'Unauthorized' 
+            });
         }
         const decoded = jwt.verify(token, config.jwtSecretKey);
         if (!decoded || !decoded.userId) {
-            return res.status(401).json({ message: 'Unauthorized' });
+            return res.status(401).json({ 
+                message: 'Unauthorized' 
+            });
         }
         req.user = decoded;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized', error: error.message });
+        return res.status(401).json({ 
+            message: 'Unauthorized', 
+            error: error.message 
+        });
     }
 }
 
